e18334f091989c34b545734bc395a6ece324e996
jiang1.li <jiang1.li@intel.com>
2022-11-17 17:21:40 +0000

    Support CodegenContext.

--- cider/function/scalar/RuntimeFunctions.cpp
+++ cider/function/scalar/RuntimeFunctions.cpp
@@ -1487,3 +1487,4 @@ extern "C" ALWAYS_INLINE int32_t extract_str_len_arrow(int8_t* offset_buffer,
 }
 
 #include "function/aggregate/CiderRuntimeFunctions.h"
+#include "exec/nextgen/context/ContextRuntimeFunctions.h"
